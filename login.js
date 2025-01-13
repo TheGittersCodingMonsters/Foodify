b92510b02464c4ccfdd09f20d1111b0947eb56dc
Belen L <belenjune@gmail.com>
2025-01-13 14:07:57 +0100

    comienzo const js

---
+++ login.js
@@ -0,0 +1,6 @@
+const google= document.getElementById('google')
+const login= document.getElementById('login')
+const usuario= document.getElementById('usuario')
+const contraseña= document.getElementById('contraseña')
+
+//Function
