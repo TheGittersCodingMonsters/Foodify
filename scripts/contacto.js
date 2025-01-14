c6575b1a925502f88bc509ab7167a3e3b6b7b28a
igoribon <igoribon@gmail.com>
2025-01-14 12:44:57 +0100

    campos de contacto terminados. script de contacto terminado. Los datos los lleva a consola. RGPD cambiada a Foodify.

--- scripts/contacto.js
+++ scripts/contacto.js
@@ -1,7 +1,13 @@
-formContacto = document.getElementById ("formContacto");
-enviarButton = document.getElementById ("enviarButton");
 
-enviarButton.addEventListener ("click", function(){
-    let
+        enviar = document.getElementById('enviarButton')
+        enviar.addEventListener('click', function() {
+            const form = document.getElementById('formContacto');
+            const formDatos = new FormData(form);
+            const jsonData = {};
 
-})
+            formDatos.forEach((value, key) => {
+                jsonData[key] = value;
+            });
+
+            console.log(JSON.stringify(jsonData));
+        });
