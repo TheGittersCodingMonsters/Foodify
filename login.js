7a9c2350e88c82db1d24d2a9c865ec120899ff7e
Belen L <belenjune@gmail.com>
2025-01-14 13:46:14 +0100

    el login del script funciona correctamente queda pendiente jacer el script para crear cuenta

--- login.js
+++ login.js
@@ -1,6 +1,27 @@
-const google= document.getElementById('google')
-const login= document.getElementById('login')
-const usuario= document.getElementById('usuario')
-const contraseña= document.getElementById('contraseña')
+const google = document.getElementById('google');
+const login = document.getElementById('login');
+const usuario = document.getElementById('usuario');
+const contraseña = document.getElementById('password');
 
-//Function
+//CONST USUARIO
+const usuarios = [
+    {usuario: "usuario1", password: "contraseña1"},
+    {usuario: "usuario2", password: "contraseña2"}
+];
+
+// Function inicio sesión
+function iniciarsesion() {
+    const usuarioInput = usuario.value;
+    const passwordInput = password.value;
+
+    const usuariovalido = usuarios.find(u => u.usuario === usuarioInput && u.password === passwordInput);
+    
+    if (usuariovalido) {
+        alert('ERES TU :)');
+    } else {
+        alert('NO ERES TU :(');
+    }
+}
+
+// EVENTO INICIO SESION
+login.addEventListener('click', iniciarsesion);
